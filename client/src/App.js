e4bb66ab1f4dd7837d6e9c20d9e33bafd4a80685
siva@2204 <sivaraman26r@gmail.com>
2020-12-11 22:53:56 +0530

    Add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
-import getWeb3 from "./getWeb3";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Web3ContextProvider from "./context/Web3Context";
 
-function App () {
-  return <h1>hello world</h1>
+function App() {
+  return (
+    <Web3ContextProvider>
+      <Router>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route exact path="/">
+            {/* <Home /> */}
+            <p>home</p>
+          </Route>
+          <Route exact path="/post">
+            <p>post page</p>
+          </Route>
+          <Route exact path="/post/:id">
+            <p>hey im the report</p>
+          </Route>
+        </Switch>
+      </Router>
+    </Web3ContextProvider>
+  );
 }
-
 
 export default App;
