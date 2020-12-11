c178652a8cedad059184f8ff627825a081f47072
siva@2204 <sivaraman26r@gmail.com>
2020-12-11 15:30:52 +0530

    Add Report Contract

---
+++ contracts/Report.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Reports {
+    uint256 public totalReports;
+
+    struct Report {
+        string title;
+        string report;
+        string location;
+        uint256 fileHash;
+        uint256 upVote;
+        uint256 downVote;
+    }
+
+    Report[] public reports;
+
+    constructor() public {
+        totalReports = 0;
+    }
+}
