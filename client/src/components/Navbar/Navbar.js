0e3396727225fe4ad98d3fb84f42c64fcb1a871b
siva@2204 <sivaraman26r@gmail.com>
2020-12-13 21:05:49 +0530

    navbar route setup

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,45 +1,58 @@
-import React from 'react'
-//import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-import './Navstyle.css'
+import "./Navstyle.css";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-      textAlign: 'left'
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+  },
+}));
 
 function Navbar() {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-
-        <AppBar position="static" className="bar">
-            <Toolbar>
-                <Typography variant="h3" className={classes.title}>
-                <Button color="inherit" className='text'>Election Proctor</Button>
-                </Typography>
-                <Button color="inherit" className='text'>Post</Button>
-                <Button color="inherit" className='text'>View Posts</Button>
-                <Button color="inherit" className='text'>Register</Button>
-            </Toolbar>
-        </AppBar>
-
-    );
-};
+  return (
+    <AppBar position="static" className="bar">
+      <Toolbar>
+        <Typography variant="h3" className={classes.title}>
+          <Link to="/">
+            <Button color="inherit" className="text">
+              Election Proctor
+            </Button>
+          </Link>
+        </Typography>
+        <Link to="/post">
+          <Button color="inherit" className="text">
+            Feeds
+          </Button>
+        </Link>
+        <Link to="/uploadpost">
+          <Button color="inherit" className="text">
+            Report
+          </Button>
+        </Link>
+        <Link>
+          <Button color="inherit" className="text">
+            Register
+          </Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
 export default Navbar;
