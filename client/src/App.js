b33f31e13def5eb11690e7a68a33a97196769378
Sneha <sneharmkrshnn@gmail.com>
2020-12-13 16:53:14 +0530

    add upload form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Web3ContextProvider from "./context/Web3Context";
-import PostReportForm from "./PostReportForm";
+import PostReportForm from "./components/Form/PostReportForm";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
