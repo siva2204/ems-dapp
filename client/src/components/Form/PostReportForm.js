49eb2251a0b487f5a5c53daf20020eb1e76f7e84
siva@2204 <sivaraman26r@gmail.com>
2020-12-14 16:30:12 +0530

    Add voting integration

--- client/src/components/Form/PostReportForm.js
+++ client/src/components/Form/PostReportForm.js
@@ -1,116 +1,132 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { Web3Context } from "../../context/Web3Context";
-import styles from './style.module.css';
+import styles from "./style.module.css";
 
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import MuiAlert from '@material-ui/lab/Alert';
-import { TextField, Button, Snackbar } from '@material-ui/core';
+import MuiAlert from "@material-ui/lab/Alert";
+import { TextField, Button, Snackbar } from "@material-ui/core";
 
-import Spinner from '../Spinner/Spinner.js'
+import Spinner from "../Spinner/Spinner.js";
 
 const theme = createMuiTheme({
   palette: {
-      primary:{
-          main: "#ff3333"
-      },
-      secondary:{
-          main: "#ff3333"
-      },
-      type: "light"
-  }
+    primary: {
+      main: "#ff3333",
+    },
+    secondary: {
+      main: "#ff3333",
+    },
+    type: "light",
+  },
 });
 
-
 //Declare IPFS
-const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) 
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
 
 function PostReportForm() {
-    const { accts,ins } = useContext(Web3Context);
-    let history = useHistory();
+  const { accts, ins } = useContext(Web3Context);
+  let history = useHistory();
 
-    const[title,setTitle] = useState('');
-    const[report,setReport] = useState('');
-    const[location,setLocation] = useState('');
-    const[file,setFile] = useState({});
-    const[fileName, setFileName] = useState('');
-    const[open, setOpen] = useState(false);
-    const[open1, setOpen1] = useState(false);
-    const[open2, setOpen2] = useState(false);
-    const[isLoad, setLoad] = userState(false);
+  const [title, setTitle] = useState("");
+  const [report, setReport] = useState("");
+  const [location, setLocation] = useState("");
+  const [file, setFile] = useState({});
+  const [fileName, setFileName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [isLoad, setLoad] = useState(false);
 
-    // listening to event from blockchain i.e contract
-    if(ins.events){
-      ins.events.reportUploaded().on("data",(e) => console.log(e,"updated successfully!"));
+  // listening to event from blockchain i.e contract
+  if (ins.events) {
+    ins.events
+      .reportUploaded()
+      .on("data", (e) => console.log(e, "updated successfully!"));
+  }
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
-
-    function Alert(props) {
-      return <MuiAlert elevation={6} variant="filled" {...props} />;
+    setOpen(false);
+  };
+  const handleClose1 = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
-     
-    const handleClose = (event, reason) => {
-      if (reason === 'clickaway') { return; }
-      setOpen(false);
-    };
-    const handleClose1 = (event, reason) => {
-      if (reason === 'clickaway') { return; }
-      setOpen1(false);
-    };
-    const handleClose2 = (event, reason) => {
-      if (reason === 'clickaway') { return; }
-      setOpen2(false);
-    };
+    setOpen1(false);
+  };
+  const handleClose2 = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen2(false);
+  };
 
-    function fileToBuffer (e) {
-      e.preventDefault();
-      const uploadedFile = e.target.files[0];
-      const ext = uploadedFile.name.substring(uploadedFile.name.lastIndexOf('.')+1, uploadedFile.name.length) || uploadedFile.name
-      if(ext === 'png' || ext === 'jpg' || ext === 'jpeg' || ext === 'mp4'){
-        setFileName(uploadedFile.name)
-        const reader = new window.FileReader()
-        reader.readAsArrayBuffer(uploadedFile)
+  function fileToBuffer(e) {
+    e.preventDefault();
+    const uploadedFile = e.target.files[0];
+    if (uploadedFile) {
+      const ext =
+        uploadedFile.name.substring(
+          uploadedFile.name.lastIndexOf(".") + 1,
+          uploadedFile.name.length
+        ) || uploadedFile.name;
+      if (ext === "png" || ext === "jpg" || ext === "jpeg" || ext === "mp4") {
+        setFileName(uploadedFile.name);
+        const reader = new window.FileReader();
+        reader.readAsArrayBuffer(uploadedFile);
         reader.onloadend = () => {
-            setFile(reader.result);
-        }
-      }
-      else{
-        setOpen(true)
+          setFile(reader.result);
+        };
+      } else {
+        setOpen(true);
       }
     }
+  }
 
-    async function uploadReport(e) {
-        e.preventDefault();
-        if(!fileName){
-          setOpen1(true)
-        }
-        else{
-          console.log("uploading");
-          setLoad(true)
-          try {
-            const response = await ipfs.add(file);
-            console.log(response);
-            console.log(`https://ipfs.infura.io/ipfs/${response.path}`);
-            // update in blockchain i.e contract
-            await ins.methods.addReport(title,report,location,response.path).send({from:accts});
-            console.log("uploaded + saved");
-            setLoad(false)
-            history.push('/feed')
-          } catch (error) {
-            setLoad(false)
-            setOpen2(true)
-            console.log(error);
-          }
-        }
+  async function uploadReport(e) {
+    e.preventDefault();
+    if (!fileName) {
+      setOpen1(true);
+    } else {
+      console.log("uploading");
+      setLoad(true);
+      try {
+        const response = await ipfs.add(file);
+        console.log(response);
+        console.log(`https://ipfs.infura.io/ipfs/${response.path}`);
+        // update in blockchain i.e contract
+        await ins.methods
+          .addReport(title, report, location, response.path)
+          .send({ from: accts });
+        console.log("uploaded + saved");
+        setLoad(false);
+        history.push("/feed");
+      } catch (error) {
+        setLoad(false);
+        setOpen2(true);
+        console.log(error);
+      }
     }
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <h3 className={styles.h3}>Post Upload</h3>
-      {isLoad?<Spinner />: ''}
-      <form onSubmit = {uploadReport} className={styles.form}>
-          
+      {isLoad ? <Spinner /> : ""}
+      <form onSubmit={uploadReport} className={styles.form}>
         <TextField
           className={styles.textField}
           id="title"
@@ -120,8 +136,8 @@ function PostReportForm() {
           variant="outlined"
           required
           label="Title"
-        /> <br/>
-
+        />{" "}
+        <br />
         <TextField
           className={styles.textField}
           id="report"
@@ -132,8 +148,8 @@ function PostReportForm() {
           type="text"
           variant="outlined"
           label="Report"
-        /> <br/>
-
+        />{" "}
+        <br />
         <TextField
           className={styles.textField}
           id="location"
@@ -142,34 +158,35 @@ function PostReportForm() {
           type="text"
           variant="outlined"
           label="Location"
-        /> <br/>
-
-        <input style={{ display: 'none' }}
+        />{" "}
+        <br />
+        <input
+          style={{ display: "none" }}
           id="file"
-          type="file" 
-          hidden 
-          onChange = {fileToBuffer} 
-        /> 
+          type="file"
+          hidden
+          onChange={fileToBuffer}
+        />
         <label htmlFor="file">
-          <Button 
-            component="span" 
-            variant="contained"
-          >
-            Upload Image or Audio
+          <Button component="span" variant="contained">
+            Upload Image or Vedio
           </Button>
-          <br/>{fileName}
-        </label> <br/><br/>
-        <Button 
-          id="post-commitment" 
+          <br />
+          {fileName}
+        </label>{" "}
+        <br />
+        <br />
+        <Button
+          id="post-commitment"
           color="primary"
-          type="submit" 
+          type="submit"
           variant="contained"
         >
           Post
         </Button>
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
           <Alert onClose={handleClose} severity="error">
-            Upload only images or audio files!
+            Upload only images or vedio files!
           </Alert>
         </Snackbar>
         <Snackbar open={open1} autoHideDuration={6000} onClose={handleClose1}>
--- client/src/components/Report/Report.js
+++ client/src/components/Report/Report.js
@@ -1,75 +1,96 @@
-import React,{useState,useContext,useEffect} from 'react';
-import {useParams} from 'react-router-dom';
-import{Web3Context} from "../../context/Web3Context";
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Web3Context } from "../../context/Web3Context";
 import axios from "axios";
-import LikeButton from './LikeButton';
+import LikeButton from "./LikeButton";
 import DisLikeButton from "./DisLikeButton";
 
-
 export default function Report() {
-    let {id} = useParams();
-    const { accts,ins } = useContext(Web3Context);
-    const[report,setReport] = useState({});
-    const[Media,setMedia] = useState(false);
-    const[like,setLike]  = useState();
-    const[disLike,setDisLike] = useState();
+  let { id } = useParams();
+  const { accts, ins } = useContext(Web3Context);
+  const [report, setReport] = useState({});
+  const [Media, setMedia] = useState(false);
+  const [like, setLike] = useState();
+  const [disLike, setDisLike] = useState();
 
-    async function upVote() {
-        try {
-            const res = await ins.methods.upVoteReport(id).send({from:accts});
-            console.log(res);
-        } catch (error) {
-            console.log(error);
-        }
-      
+  async function upVote() {
+    try {
+      await ins.methods.upVoteReport(id).send({ from: accts });
+      const response = await ins.methods.reports(id).call();
+      setLike(response.upVote);
+    } catch (error) {
+      console.log(error);
     }
-   
-    const getReport =  async ()=>{
-       if(ins.methods){
-            const response = await ins.methods.reports(id).call();
-            setReport(response);
-            setLike(response.upVote);
-            setDisLike(response.downVote);
-         
-            const {headers} = await axios.get(`https://ipfs.infura.io/ipfs/${response.fileHash}`);
-            const myFileFormat = headers["content-type"];
+  }
 
-            if (myFileFormat === "video/mp4") {
-                let media = (
-                  <video width="320" height="240" controls>
-                    <source src={`https://ipfs.infura.io/ipfs/${response.fileHash}`} type="video/mp4" />
-                  </video>
-                );
-                setMedia(media);
-              } else if (myFileFormat === "image/png" || myFileFormat === "image/jpeg" || myFileFormat === "image/jpg") {
-                let media = (
-                  <a src={`https://ipfs.infura.io/ipfs/${response.fileHash}`} target="_blank">
-                    <img src={`https://ipfs.infura.io/ipfs/${response.fileHash}`} width="320" height="240"></img>
-                  </a>
-                );
-                setMedia(media);
-              }
-
-        } 
+  async function downVote() {
+    try {
+      await ins.methods.downVoteReport(id).send({ from: accts });
+      const response = await ins.methods.reports(id).call();
+      setLike(response.downVote);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  const getReport = async () => {
+    if (ins.methods) {
+      const response = await ins.methods.reports(id).call();
+      setReport(response);
+      setLike(response.upVote);
+      setDisLike(response.downVote);
 
-    useEffect(()=>{
-        getReport();
-    },[ins]);
+      const { headers } = await axios.get(
+        `https://ipfs.infura.io/ipfs/${response.fileHash}`
+      );
+      const myFileFormat = headers["content-type"];
 
-    
-        return (
-        <>
-          <h1>{report.title}</h1>
-          <span>{report.location}</span>
-          <div>{Media? Media :"loading"}</div>
-          <p>{report.report}</p>
-          <div>
-              <DisLikeButton dislike={like}/>
-              <LikeButton like={disLike} upVote={upVote}/>
-          </div>
-
-        </>
+      if (myFileFormat === "video/mp4") {
+        let media = (
+          <video width="320" height="240" controls>
+            <source
+              src={`https://ipfs.infura.io/ipfs/${response.fileHash}`}
+              type="video/mp4"
+            />
+          </video>
         );
+        setMedia(media);
+      } else if (
+        myFileFormat === "image/png" ||
+        myFileFormat === "image/jpeg" ||
+        myFileFormat === "image/jpg"
+      ) {
+        let media = (
+          <a
+            src={`https://ipfs.infura.io/ipfs/${response.fileHash}`}
+            target="_blank"
+          >
+            <img
+              src={`https://ipfs.infura.io/ipfs/${response.fileHash}`}
+              width="320"
+              height="240"
+            ></img>
+          </a>
+        );
+        setMedia(media);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getReport();
+  }, [ins]);
+
+  return (
+    <>
+      <h1>{report.title}</h1>
+      <span>{report.location}</span>
+      <div>{Media ? Media : "loading"}</div>
+      <p>{report.report}</p>
+      <div>
+        <DisLikeButton dislike={disLike} downVote={downVote} />
+        <LikeButton like={like} upVote={upVote} />
+      </div>
+    </>
+  );
 }
